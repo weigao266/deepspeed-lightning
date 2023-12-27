33e8d98915900e559dccaf06b8c51d3d36dab11b
Weigao Sun <sunweigao@outlook.com>
2023-12-27 16:02:15 +0800

    now broadcast and split samples instead of embed, debug for sp size smaller than world size

--- deepspeed/comm/seq_parallel_manager.py
+++ deepspeed/comm/seq_parallel_manager.py
@@ -1,7 +1,11 @@
 """Communication manager for lightning attention data parallel + sequence parallel."""
 
+import io
 import torch
+import torch.distributed as dist
 from .seq_parallel_utils import GlobalMemoryBuffer
+from dataclasses import dataclass
+from typing import Any, Dict, List, Mapping, Optional
 
 # Data parallel group that the current rank belongs to.
 _DATA_PARALLEL_GROUP = None
@@ -234,9 +238,142 @@ def destroy_parallel_groups():
     global _GLOBAL_MEMORY_BUFFER
     _GLOBAL_MEMORY_BUFFER = None
     
+def get_rank(group):
+    return dist.get_rank(group=group)
 
-def broadcast_tensor(tensor, src, group):
+def broadcast(tensor, src, group):
     torch.distributed.broadcast(tensor, src=src, group=group)
+
+def broadcast_tensors(
+    tensors: Optional[List[torch.Tensor]],
+    src_rank: int,
+    group: object,
+    dist_device: Optional[torch.device] = None,
+) -> List[torch.Tensor]:
+    """
+    Broadcasts a list of tensors without other (non-src) ranks needing to know
+    the dtypes/shapes of the tensors.
+    """
+    if dist_device is None:
+        if torch.distributed.get_backend(group) == "nccl":
+            dist_device = torch.device("cuda")
+        else:
+            dist_device = torch.device("cpu")
+
+    # share metadata first to simplify transfer
+    is_src_rank = get_rank(group) == src_rank
+    if is_src_rank:
+        # We only want to communicate device type ie (cpu vs cuda) and not the index of cuda.
+        metadata = [
+            {"size": t.size(), "dtype": t.dtype, "device": torch.device(t.device.type)}
+            for t in tensors
+        ]
+        metadata = _broadcast_object_slow(metadata, src_rank, group, dist_device)
+    else:
+        metadata = _broadcast_object_slow(None, src_rank, group, dist_device)
+
+    out_tensors = []
+    for i, meta in enumerate(metadata):
+        if is_src_rank:
+            tensor = tensors[i]
+            broadcast(tensors[i].to(dist_device), src=src_rank, group=group)
+        else:
+            tensor = torch.zeros(
+                [meta["size"].numel()], dtype=meta["dtype"], device=dist_device
+            )
+            broadcast(tensor, src=src_rank, group=group)
+
+        tensor = tensor.view(meta["size"]).to(meta["device"])
+        out_tensors.append(tensor)
+    return out_tensors
+
+
+def broadcast_object(
+    obj: Any,
+    src_rank: int,
+    group: object,
+    dist_device: Optional[torch.device] = None,
+) -> Any:
+    """Broadcast an arbitrary Python object to other workers."""
+    if dist_device is None:
+        if torch.distributed.get_backend(group) == "nccl":
+            dist_device = torch.device("cuda")
+        else:
+            dist_device = torch.device("cpu")
+
+    if get_rank(group) == src_rank:
+        # split the tensors from the non-tensors so we can broadcast them
+        # directly, avoiding unnecessary serialization/deserialization
+        tensors = []
+        obj = _split_tensors_from_obj(obj, tensors)
+        obj = _broadcast_object_slow(obj, src_rank, group, dist_device)
+        tensors = broadcast_tensors(tensors, src_rank, group, dist_device)
+    else:
+        obj = _broadcast_object_slow(None, src_rank, group, dist_device)
+        tensors = broadcast_tensors(None, src_rank, group, dist_device)
+    return _put_tensors_in_obj(obj, tensors)
+
+
+def _broadcast_object_slow(
+    obj: Any,
+    src_rank: int,
+    group: object,
+    dist_device: torch.device,
+) -> Any:
+    if get_rank(group) == src_rank:
+        # Emit data
+        buffer = io.BytesIO()
+        torch.save(obj, buffer)
+        buffer = torch.ByteTensor(buffer.getbuffer()).to(dist_device)
+        length = torch.LongTensor([len(buffer)]).to(dist_device)
+        broadcast(length, src=src_rank, group=group)
+        broadcast(buffer, src=src_rank, group=group)
+    else:
+        # Fetch from the source
+        length = torch.LongTensor([0]).to(dist_device)
+        broadcast(length, src=src_rank, group=group)
+        buffer = torch.ByteTensor(int(length.item())).to(dist_device)
+        broadcast(buffer, src=src_rank, group=group)
+        buffer = io.BytesIO(buffer.cpu().numpy())
+        obj = torch.load(buffer, map_location="cpu")
+    return obj
+
+
+@dataclass(frozen=True)
+class _TensorPlaceholder:
+    index: int
+
+
+def _split_tensors_from_obj(obj: Any, tensors: List[torch.Tensor]) -> Any:
+    if torch.is_tensor(obj):
+        placeholder = _TensorPlaceholder(index=len(tensors))
+        tensors.append(obj)
+        return placeholder
+    elif isinstance(obj, dict):
+        return {k: _split_tensors_from_obj(v, tensors) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [_split_tensors_from_obj(v, tensors) for v in obj]
+    elif isinstance(obj, tuple):
+        return tuple(_split_tensors_from_obj(v, tensors) for v in obj)
+    elif isinstance(obj, set):
+        return {_split_tensors_from_obj(v, tensors) for v in obj}
+    else:
+        return obj
+
+
+def _put_tensors_in_obj(obj: Any, tensors: List[torch.Tensor]) -> Any:
+    if isinstance(obj, _TensorPlaceholder):
+        return tensors[obj.index]
+    elif isinstance(obj, dict):
+        return {k: _put_tensors_in_obj(v, tensors) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [_put_tensors_in_obj(v, tensors) for v in obj]
+    elif isinstance(obj, tuple):
+        return tuple(_put_tensors_in_obj(v, tensors) for v in obj)
+    elif isinstance(obj, set):
+        return {_put_tensors_in_obj(v, tensors) for v in obj}
+    else:
+        return obj
 
 def split_along_first_dim(input_):
     """Split the tensor along its first dimension and keep the
@@ -251,17 +388,40 @@ def split_along_first_dim(input_):
     rank = get_sequence_parallel_rank()
     src_rank = get_sequence_parallel_src_rank()
     group = get_sequence_parallel_group()
-    broadcast_tensor(input_, src_rank, group=group)
+    broadcast(input_, src_rank, group=group)
 
     # Split along first dimension.
     dim_size = input_.size()[0]
     assert (
         dim_size % world_size == 0
-    ), "First dimension of the tensor should be divisible by tensor parallel size"
+    ), "First dimension of the tensor should be divisible by sequence parallel size"
     local_dim_size = dim_size // world_size
     dim_offset = rank * local_dim_size
 
     output = input_[dim_offset : dim_offset + local_dim_size].contiguous()
+
+    return output
+
+
+def split_along_second_dim(input_):
+    """Split the tensor along its second dimension and keep the
+    corresponding slice."""
+
+    world_size = get_sequence_parallel_world_size()
+    # Bypass the function if we are using only 1 GPU.
+    if world_size == 1:
+        return input_
+
+    rank = get_sequence_parallel_rank()
+    # Split along second dimension.
+    dim_size = input_.size()[1]
+    assert (
+        dim_size % world_size == 0
+    ), "Second dimension of the tensor should be divisible by sequence parallel size"
+    local_dim_size = dim_size // world_size
+    dim_offset = rank * local_dim_size
+
+    output = input_[:, dim_offset : dim_offset + local_dim_size].contiguous()
 
     return output
 
